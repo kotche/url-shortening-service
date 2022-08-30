a90ec3df28b7f0d2321a7ccb2f49aaf0a97291f3
Kirill Che <kot313@gmail.com>
2022-08-30 17:45:08 +0300

    inc_21:test bug fix 2

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -8,11 +8,17 @@ import (
 
 // Config sets the basic settings
 type Config struct {
-	ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080" json:"server_address"`
-	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080" json:"base_url"`
-	FilePath    string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
-	DBConnect   string `env:"DATABASE_DSN" json:"database_dsn"`
-	EnableHTTPS bool   `env:"ENABLE_HTTPS" json:"enable_https"`
+	//ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080" json:"server_address"`
+	//BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080" json:"base_url"`
+	//FilePath    string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
+	//DBConnect   string `env:"DATABASE_DSN" json:"database_dsn"`
+	//EnableHTTPS bool   `env:"ENABLE_HTTPS" json:"enable_https"`
+
+	ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
+	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	FilePath    string `env:"FILE_STORAGE_PATH"`
+	DBConnect   string `env:"DATABASE_DSN"`
+	EnableHTTPS bool   `env:"ENABLE_HTTPS"`
 
 	HostWhitelist []string `json:"hostWhitelist"`
 }

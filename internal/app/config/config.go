1dda2ef286042a3e89b7d65ce165dac3a2bef4ac
Kirill Che <kot313@gmail.com>
2022-08-30 17:09:10 +0300

    inc_21:changed priority read env+flags+config file

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,35 +1,68 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"log"
+	"os"
 
 	"github.com/caarlos0/env/v6"
 )
 
 // Config sets the basic settings
 type Config struct {
-	ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FilePath    string `env:"FILE_STORAGE_PATH"`
-	DBConnect   string `env:"DATABASE_DSN"`
-	EnableHTTPS bool   `env:"ENABLE_HTTPS"`
+	ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080" json:"server_address"`
+	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080" json:"base_url"`
+	FilePath    string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
+	DBConnect   string `env:"DATABASE_DSN" json:"database_dsn"`
+	EnableHTTPS bool   `env:"ENABLE_HTTPS" json:"enable_https"`
 
-	HostWhitelist []string
+	HostWhitelist []string `json:"hostWhitelist"`
 }
 
+//NewConfig priority: env and flag are on the same level, the configuration file is below
+//example config file flag: -c ./internal/app/config/config.json
 func NewConfig() (*Config, error) {
+	var serverAddr, baseURL, filePath, dbConnect, enableHTTPSStr, configFilePath string
+
+	regStringVar(&serverAddr, "a", serverAddr, "server address")
+	regStringVar(&baseURL, "b", baseURL, "base url")
+	regStringVar(&filePath, "f", filePath, "file storage path")
+	regStringVar(&dbConnect, "d", dbConnect, "database connection")
+	regStringVar(&enableHTTPSStr, "s", enableHTTPSStr, "enable HTTPS")
+	regStringVar(&configFilePath, "c", configFilePath, "config file")
+	regStringVar(&configFilePath, "config", configFilePath, "config file")
+	flag.Parse()
+
 	conf := &Config{}
+
+	if configFilePath != "" {
+		readConfigFile(conf, configFilePath)
+	}
 
 	if err := env.Parse(conf); err != nil {
 		return nil, err
 	}
 
-	regStringVar(&conf.ServerAddr, "a", conf.ServerAddr, "server address")
-	regStringVar(&conf.BaseURL, "b", conf.BaseURL, "base url")
-	regStringVar(&conf.FilePath, "f", conf.FilePath, "file storage path")
-	regStringVar(&conf.DBConnect, "d", conf.DBConnect, "database connection")
-	regStringBool(&conf.EnableHTTPS, "s", conf.EnableHTTPS, "enable HTTPS")
-	flag.Parse()
+	if serverAddr != "" {
+		conf.ServerAddr = serverAddr
+	}
+	if baseURL != "" {
+		conf.BaseURL = baseURL
+	}
+	if filePath != "" {
+		conf.FilePath = filePath
+	}
+	if dbConnect != "" {
+		conf.DBConnect = dbConnect
+	}
+	if enableHTTPSStr == "true" {
+		conf.EnableHTTPS = true
+	}
+
+	if err := env.Parse(conf); err != nil {
+		return nil, err
+	}
 
 	return conf, nil
 }
@@ -39,9 +72,15 @@ func regStringVar(p *string, name string, value string, usage string) {
 		flag.StringVar(p, name, value, usage)
 	}
 }
-
-func regStringBool(p *bool, name string, value bool, usage string) {
-	if flag.Lookup(name) == nil {
-		flag.BoolVar(p, name, value, usage)
+func readConfigFile(conf *Config, configFilePath string) {
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		log.Printf("file config open error: %s", err)
+	}
+	defer configFile.Close()
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&conf)
+	if err != nil {
+		log.Printf("file config decode error: %s", err)
 	}
 }

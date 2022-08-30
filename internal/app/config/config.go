e7a362e6595ffb1a986e7ccbb969368617a08a14
Kirill Che <kot313@gmail.com>
2022-08-30 17:51:02 +0300

    inc_21: bug fix

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,24 +1,21 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"log"
+	"os"
 
 	"github.com/caarlos0/env/v6"
 )
 
 // Config sets the basic settings
 type Config struct {
-	//ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080" json:"server_address"`
-	//BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080" json:"base_url"`
-	//FilePath    string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
-	//DBConnect   string `env:"DATABASE_DSN" json:"database_dsn"`
-	//EnableHTTPS bool   `env:"ENABLE_HTTPS" json:"enable_https"`
-
-	ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FilePath    string `env:"FILE_STORAGE_PATH"`
-	DBConnect   string `env:"DATABASE_DSN"`
-	EnableHTTPS bool   `env:"ENABLE_HTTPS"`
+	ServerAddr  string `env:"SERVER_ADDRESS" envDefault:"localhost:8080" json:"server_address"`
+	BaseURL     string `env:"BASE_URL" envDefault:"http://localhost:8080" json:"base_url"`
+	FilePath    string `env:"FILE_STORAGE_PATH" json:"file_storage_path"`
+	DBConnect   string `env:"DATABASE_DSN" json:"database_dsn"`
+	EnableHTTPS bool   `env:"ENABLE_HTTPS" json:"enable_https"`
 
 	HostWhitelist []string `json:"hostWhitelist"`
 }
@@ -39,9 +36,9 @@ func NewConfig() (*Config, error) {
 
 	conf := &Config{}
 
-	//if configFilePath != "" {
-	//	readConfigFile(conf, configFilePath)
-	//}
+	if configFilePath != "" {
+		readConfigFile(conf, configFilePath)
+	}
 
 	if err := env.Parse(conf); err != nil {
 		return nil, err
@@ -63,10 +60,6 @@ func NewConfig() (*Config, error) {
 		conf.EnableHTTPS = true
 	}
 
-	if err := env.Parse(conf); err != nil {
-		return nil, err
-	}
-
 	return conf, nil
 }
 
@@ -76,15 +69,15 @@ func regStringVar(p *string, name string, value string, usage string) {
 	}
 }
 
-//func readConfigFile(conf *Config, configFilePath string) {
-//	configFile, err := os.Open(configFilePath)
-//	if err != nil {
-//		log.Printf("file config open error: %s", err)
-//	}
-//	defer configFile.Close()
-//	jsonParser := json.NewDecoder(configFile)
-//	err = jsonParser.Decode(&conf)
-//	if err != nil {
-//		log.Printf("file config decode error: %s", err)
-//	}
-//}
+func readConfigFile(conf *Config, configFilePath string) {
+	configFile, err := os.Open(configFilePath)
+	if err != nil {
+		log.Printf("file config open error: %s", err)
+	}
+	defer configFile.Close()
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&conf)
+	if err != nil {
+		log.Printf("file config decode error: %s", err)
+	}
+}
